1c44445044696b80ecf52052a7d9c63a9ab00e52
MorganStefanie <109448525+MorganStefanie@users.noreply.github.com>
2023-03-18 15:19:59 -0500

    Update Number guessing.cpp

--- Number guessing.cpp
+++ Number guessing.cpp
@@ -1,10 +1,33 @@
-using namespace STD
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-int main(){
+int main()
+{
+	int arr [23];
+	int n=23;
 
-while (true)
+	return 0;
+}
+{
+int main()
+	
+	int num, guess, tries = 0;
+	srand(time(0)); 
+	num = rand() % 100; 
+	cout << "Starting the Number Game\n\n";
 
-srand(time(51))
-int secretnumber = 1 + (rand()%100);
-int playerchoice;
+	while (guess!= num); {
+		cout << "Enter a guess between 0 and 100: "; 
+		cin >> guess; 
+		if (guess > num)
+			cout << "Too high!\n\n";
+		else if (guess < num)
+			cout << "Too low!\n\n";
+		else  
+			cout << "\nCorrect! You got it in " << tries << " guesses!\n";
+	}  
+
+	return 0; 
 }
